fc25a47e7ad231107b54173fb7122cf429291dc2
Lucas Santiago Borgna <lucas.borgna.16@ucl.ac.uk>
2017-11-16 14:11:11 +0000

    reverted back

--- lab1.v
+++ lab1.v
@@ -12,8 +12,7 @@ module lab1(
     assign led[0] = ~swt[0];
     assign led[1] = swt[1] & ~swt[2];
     assign led[3] = swt[2] & swt[3];
-    assign led[2] = led[1] | led[3];
-    assign led[4] = ~swt[4];    
-    assign led[7:5] = swt[7:5];
+    assign led[2] = led[1] | led[3];    
+    assign led[7:4] = swt[7:4];
     
 endmodule
